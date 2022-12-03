10770426218a46f62c7a54c26a69b2c390a96df3
Leira23 <61948468+Niburr@users.noreply.github.com>
2022-12-03 03:45:16 +0000

    Added new files

---
+++ Unit 11/action_page.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>replit</title>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<h1>
+  Your information has been received.
+</h1>
+<form action="/index.html">
+  <input type="submit" value="Back">
+</form>
+</body>
+
+</html>
